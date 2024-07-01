956fe39850af7220cb0a78f49835432993f681e9
ZedOweYou <chenxi.zou@gmail.com>
2024-07-01 18:44:54 -0400

    env setup test file

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 18:28:03 2024
+
+@author: chenx
+"""
+
+# setup.py
+# just testing kx licensing and environment
+
+import pykx as kx
+from pykx import q
+
+# Print a welcome message from q
+q("system \"echo 'Hello, Q!'\"")
+
+#create a table with a large set of random numbers
+q("col1: 1000000?100")
+q("col2: 1000000?120")
+q("t:([]col1:col1;col2:col2)")
+# Fetch the table back into Python
+table = q("5# select col1,col2 from t")
+
+# Print the table
+print(table)
