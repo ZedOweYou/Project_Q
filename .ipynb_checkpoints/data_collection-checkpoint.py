fc9f1e874990834356bd42f502f1780c881e3825
ZedOweYou <chenxi.zou@gmail.com>
2024-07-01 22:44:09 -0400

    data_collection.py working

--- .ipynb_checkpoints/data_collection-checkpoint.py
+++ .ipynb_checkpoints/data_collection-checkpoint.py
@@ -8,21 +8,21 @@ Created on Mon Jul  1 18:51:58 2024
 #data_collection.py
 
 #external
+import requests
 import pykx as kx
 from pykx import q
 import yfinance as yf
+import pandas as pd
+import numpy as np
 
 #internal
 from tooling import pandas_to_kdb
 
-def get_sym_data(symbol:str) -> kx.wrappers.Table:
-    sym = yf.Ticker(symbol)
-    df = sym.hist(period="1d",interval="1m",actions=False).reset_index()
+def get_sym_data(symbols):
+    sym = yf.Ticker(symbols)
+    df = yf.download(symbols, period="1d",interval="1m",actions=False).reset_index()
     print(df.head())
     t = pandas_to_kdb(df)
     return t
-
-
-
 
 
