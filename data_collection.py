cbcc9c94fe045707324e8ed557441f5bb86c695e
ZedOweYou <chenxi.zou@gmail.com>
2024-07-02 10:46:56 -0400

    add database manager

--- data_collection.py
+++ data_collection.py
@@ -8,19 +8,30 @@ Created on Mon Jul  1 18:51:58 2024
 #data_collection.py
 
 #external
+from datetime import datetime
 from pykx.wrappers import Table
+from pykx import q
 import yfinance as yf
 
 #internal
 from tooling import pandas_to_kdb
+import database_manager as dm
 
 #pull data from yfinance convert to kdb table
-def get_sym_data(symbol:str) -> Table:
+def get_sym_data(symbol:str, start: datetime=None) -> Table:
     sym = yf.Ticker(symbol)
-    df = sym.history(period="1d",interval="1m",actions=False).reset_index()
-    print(df.head())
+    if start!=None:
+        df = sym.history(start=start,interval="1m",actions=False).reset_index()
+    else:
+        df = sym.history(period="1d",interval="1m",actions=False).reset_index()
     t = pandas_to_kdb(df)
     return t
+
+def update_sym_data(symbol:str) -> Table:
+    t = dm.read_table(symbol)
+    print(q("t"))
+    return t
+
 
 
 
---
+++ database_manager.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 08:30:52 2024
+
+@author: ZedOweYou
+"""
+
+#database_manager.py
+
+from pykx import q
+from pykx.wrappers import Table
+from pykx.wrappers import SymbolAtom
+from datetime import datetime
+
+def save_table(t: Table, sym: str) -> SymbolAtom:
+    p = ('`:data/'+sym+'/ set t')
+    return p
+
+def read_table(sym: str) -> Table:
+    q('t: get `:data/'+sym+'/')
+    return q('select from t')
+
+def latest_date(sym: str) -> datetime:
+    q('t: get `:data/'+sym+'/')
+    return q("max t`Datetime").py()
+    
