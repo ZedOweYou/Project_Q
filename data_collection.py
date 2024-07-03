bb30e3d8e83684c758bd09cb1ecc3f2f7925bb6b
ZedOweYou <chenxi.zou@gmail.com>
2024-07-02 22:23:07 -0400

    linting

--- data_collection.py
+++ data_collection.py
@@ -29,20 +29,20 @@ def get_sym_data(symbol: str,
     Retrieve historical stock data from Yahoo Finance.
 
     Parameters.
-    symbol (str): The stock symbol to retrieve data for.
+        symbol (str): The stock symbol to retrieve data for.
 
-    start (datetime, optional): The start date for the historical data.
+        start (datetime, optional): The start date for the historical data.
     If None, defaults to the past 1 day.
 
-    pandas (bool, optional): If True, return the data as a pandas DataFrame.
+        pandas (bool, optional): If True, return the data as pandas DataFrame.
     If False, convert to a kdb+ table.
 
     Returns.
-    Table or DataFrame: The historical stock data as a kdb+ table
+        Table or DataFrame: The historical stock data as a kdb+ table
     or pandas DataFrame.
 
     Raises.
-    NoData: If no data is received from Yahoo Finance.
+        NoData: If no data is received from Yahoo Finance.
     """
     sym = yf.Ticker(symbol)
     if start is not None:
@@ -63,13 +63,13 @@ def get_new_sym_data(symbol: str) -> Table:
     Retrieve new stock data for a given symbol from Yahoo Finance.
 
     Parameters.
-    symbol (str): The stock symbol to retrieve data for.
+        symbol (str): The stock symbol to retrieve data for.
 
     Returns.
-    Table: The historical stock data as a kdb+ table.
+        Table: The historical stock data as a kdb+ table.
 
     Raises.
-    NoData: If no data is received from Yahoo Finance.
+        NoData: If no data is received from Yahoo Finance.
     """
     latest_date = dm.latest_date(symbol)
     t = get_sym_data(symbol, start=latest_date)
